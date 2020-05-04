2efff5eec0d097a8835bb5215fc83e6c311e2726
Neeqstock <nicola.davanzo01@gmail.com>
2020-05-04 20:14:13 +0200

    Temp
    Temp

--- NeeqDMIs/Eyetracking/EyeX/IEyeXEyePositionBehavior.cs
+++
@@ -1,9 +0,0 @@
-﻿using EyeXFramework;
-
-namespace NeeqDMIs.Eyetracking.EyeX
-{
-    public interface IEyeXEyePositionBehavior
-    {
-        void ReceiveEyePosition(EyePositionEventArgs e);
-    }
-}
--- Netytar/Netytar/DMIbox/NetytarRack.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Netytar
-{
-    public static class NetytarRack
-    {
-        private static NetytarDMIBox dmibox = new NetytarDMIBox();
-        public static NetytarDMIBox DMIBox { get => dmibox; set => dmibox = value; }
-    }
-}
